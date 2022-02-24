75a1be36424b391c355ddb100c2c969d74c0f3ef
Alejandro Martín Fernández <alejandro.mf1002@gmail.com>
2022-02-24 15:34:07 +0100

    menu updated

--- app/src/main/java/com/alemanal/logwinder/MainActivity.java
+++ app/src/main/java/com/alemanal/logwinder/MainActivity.java
@@ -1,16 +1,27 @@
 package com.alemanal.logwinder;
 
+import com.alemanal.logwinder.TopMenu;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+public class MainActivity extends AppCompatActivity{
+    String actionActivity;
     @Override
     //union rama
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        BottomNavigationView btNav = findViewById(R.id.bottom_navigation);
+        btNav.setSelectedItemId(R.id.home);
+        TopMenu.navegacion(btNav, MainActivity.this);
     }
 }
--- app/src/main/java/com/alemanal/logwinder/Profile.java
+++ app/src/main/java/com/alemanal/logwinder/Profile.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        BottomNavigationView btNav = findViewById(R.id.bottom_navigation);
+        btNav.setSelectedItemId(R.id.profile);
+        TopMenu.navegacion(btNav, Profile.this);
         ViewPager.data.get("cv1");
         ViewPager.data.get("cv2");
         ViewPager.data.get("cv3");
