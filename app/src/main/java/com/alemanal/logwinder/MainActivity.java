67c1d8b8abf377bd27775a8f3eef6438ba694560
WOLFCRAY <alex.pallango.hinojoza@gmail.com>
2022-02-25 17:39:23 +0100

    listView lista

--- app/src/main/java/com/alemanal/logwinder/MainActivity.java
+++ app/src/main/java/com/alemanal/logwinder/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alemanal.logwinder.api.Pvpc;
@@ -24,8 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private SwipeRefreshLayout swrl;
     Pvpc pvpc;
     ArrayList<String> listaInformacionn;
+    ArrayList<Object> listaHoraI;
+    ArrayList<Object> listaPorc;
+    ArrayList<Object> listaPre;
     ListView listaMain;
     String dato;
+    double media;
+    double kilowatio;
+
+    Object[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +66,11 @@ public class MainActivity extends AppCompatActivity {
 
         return listaInformacion;
     }*/
-        listaInformacionn = new ArrayList<String>();
-        listaInformacionn.add(dato);
+       // listaInformacionn = new ArrayList<String>();
+        //listaInformacionn.add(dato);
 
-        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listaInformacionn);
-        listaMain.setAdapter(adaptador);
+        /*ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listaInformacionn);
+        listaMain.setAdapter(adaptador);*/
 
     }
 
@@ -78,14 +88,28 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Pvpc> call, Response<Pvpc> response) {
                 if (!response.isSuccessful()) {
                     Toast.makeText(MainActivity.this,"Código: " + response.code(),Toast.LENGTH_SHORT);
-                    pvpc = response.body();
-                    dato = pvpc.get0001().getHour();
-                    System.out.println(dato);
-
-
-
                     return;
                 }
+                
+                pvpc = response.body();
+                
+                
+                dato = pvpc.get0001().getHour();
+                System.out.println(dato);
+                hora();
+                porc();
+                precioK();
+
+                //customAdapter customAdapter = new customAdapter();
+
+                //listaInformacionn = new ArrayList<String>();
+                //listaInformacionn.add(dato);
+
+
+//                ArrayAdapter adaptador = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,listaInformacion);
+//                listaMain.setAdapter(adaptador);
+                customAdapter customAdapter = new customAdapter();
+                listaMain.setAdapter(customAdapter);
             }
             @Override
             public void onFailure(Call<Pvpc> call, Throwable t) {
@@ -94,10 +118,148 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void precioK() {
+        listaPre = new ArrayList<Object>();
+        listaPre.add(pvpc.get0001().getPrice());
+        listaPre.add(pvpc.get0001().getPrice());
+        listaPre.add(pvpc.get0102().getPrice());
+        listaPre.add(pvpc.get0203().getPrice());
+        listaPre.add(pvpc.get0304().getPrice());
+        listaPre.add(pvpc.get0405().getPrice());
+        listaPre.add(pvpc.get0506().getPrice());
+        listaPre.add(pvpc.get0607().getPrice());
+        listaPre.add(pvpc.get0708().getPrice());
+        listaPre.add(pvpc.get0809().getPrice());
+        listaPre.add(pvpc.get0910().getPrice());
+        listaPre.add(pvpc.get1011().getPrice());
+        listaPre.add(pvpc.get1112().getPrice());
+        listaPre.add(pvpc.get1213().getPrice());
+        listaPre.add(pvpc.get1314().getPrice());
+        listaPre.add(pvpc.get1415().getPrice());
+        listaPre.add(pvpc.get1516().getPrice());
+        listaPre.add(pvpc.get1617().getPrice());
+        listaPre.add(pvpc.get1718().getPrice());
+        listaPre.add(pvpc.get1920().getPrice());
+        listaPre.add(pvpc.get2021().getPrice());
+        listaPre.add(pvpc.get2122().getPrice());
+        listaPre.add(pvpc.get2223().getPrice());
+        listaPre.add(pvpc.get2324().getPrice());
+        double num=0;
+        for (int i=0 ; i<listaPorc.size();i++){
+            num = (double) listaPorc.get(i);
+        }
+
+    }
+
+    private void hora() {
+        listaHoraI = new ArrayList<Object>();
+        listaHoraI.add(pvpc.get0001().getHour());
+        listaHoraI.add(pvpc.get0102().getHour());
+        listaHoraI.add(pvpc.get0203().getHour());
+        listaHoraI.add(pvpc.get0304().getHour());
+        listaHoraI.add(pvpc.get0405().getHour());
+        listaHoraI.add(pvpc.get0506().getHour());
+        listaHoraI.add(pvpc.get0607().getHour());
+        listaHoraI.add(pvpc.get0708().getHour());
+        listaHoraI.add(pvpc.get0809().getHour());
+        listaHoraI.add(pvpc.get0910().getHour());
+        listaHoraI.add(pvpc.get1011().getHour());
+        listaHoraI.add(pvpc.get1112().getHour());
+        listaHoraI.add(pvpc.get1213().getHour());
+        listaHoraI.add(pvpc.get1314().getHour());
+        listaHoraI.add(pvpc.get1415().getHour());
+        listaHoraI.add(pvpc.get1516().getHour());
+        listaHoraI.add(pvpc.get1617().getHour());
+        listaHoraI.add(pvpc.get1718().getHour());
+        listaHoraI.add(pvpc.get1920().getHour());
+        listaHoraI.add(pvpc.get2021().getHour());
+        listaHoraI.add(pvpc.get2122().getHour());
+        listaHoraI.add(pvpc.get2223().getHour());
+        listaHoraI.add(pvpc.get2324().getHour());
+    }
+
+    private void porc() {
+
+        listaPorc = new ArrayList<Object>();
+        listaPorc.add(pvpc.get0001().getPrice());
+        listaPorc.add(pvpc.get0102().getPrice());
+        listaPorc.add(pvpc.get0203().getPrice());
+        listaPorc.add(pvpc.get0304().getPrice());
+        listaPorc.add(pvpc.get0405().getPrice());
+        listaPorc.add(pvpc.get0506().getPrice());
+        listaPorc.add(pvpc.get0607().getPrice());
+        listaPorc.add(pvpc.get0708().getPrice());
+        listaPorc.add(pvpc.get0809().getPrice());
+        listaPorc.add(pvpc.get0910().getPrice());
+        listaPorc.add(pvpc.get1011().getPrice());
+        listaPorc.add(pvpc.get1112().getPrice());
+        listaPorc.add(pvpc.get1213().getPrice());
+        listaPorc.add(pvpc.get1314().getPrice());
+        listaPorc.add(pvpc.get1415().getPrice());
+        listaPorc.add(pvpc.get1516().getPrice());
+        listaPorc.add(pvpc.get1617().getPrice());
+        listaPorc.add(pvpc.get1718().getPrice());
+        listaPorc.add(pvpc.get1920().getPrice());
+        listaPorc.add(pvpc.get2021().getPrice());
+        listaPorc.add(pvpc.get2122().getPrice());
+        listaPorc.add(pvpc.get2223().getPrice());
+        listaPorc.add(pvpc.get2324().getPrice());
+
+        double num;
+        double sum=0;
+
+        for (int i=0 ; i<listaPorc.size();i++){
+            num = (double) listaPorc.get(i);
+            sum+=num;
+        }
+        media = sum/listaPorc.size();
+
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
         getPosts();
+    }
+
+    class customAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+
+            return listaHoraI.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            View myView = getLayoutInflater().inflate(R.layout.lista_datos,null);
+            TextView tvHora = myView.findViewById(R.id.tvFin);
+            TextView tvporc = myView.findViewById(R.id.tvPorcentaje);
+            TextView tvprecio = myView.findViewById(R.id.tvPrecio);
+            tvHora.setSelected(true);
+            tvHora.setText(listaHoraI.get(pos).toString());
+
+            tvporc.setSelected(true);
+            double porciento = (double) listaPorc.get(pos);
+            int porcentaje= (int) ((porciento*100/media)-100);
+            tvporc.setText(porcentaje+"%");
+
+            tvprecio.setSelected(true);
+            double precio = (double) listaPre.get(pos)/1000;
+            String decimal = String.format("%.4f", precio);
+            tvprecio.setText(decimal +" €/kWh");
+            return myView;
+        }
     }
 
 
